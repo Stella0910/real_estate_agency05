5de462022def588803f32dc009c5d8801bd78bb5
Stella <aniretake0910@gmail.com>
2026-02-08 02:07:12 +0300

    add model field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -5,6 +5,14 @@ from django.utils import timezone
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    new_building = models.BooleanField(
+        verbose_name='New building',
+        choices=[(True, 'Новостройка'),
+                 (False, 'Вторичка'),
+                 (None, 'Неизвестно')],
+        default=None,
+        null=True,
+        blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
